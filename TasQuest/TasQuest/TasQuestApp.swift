c48d6820b3fca7741104842366472ad5a40cda72
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-04 23:10:40 +0900

    feat: add initial Xcode project files

---
+++ TasQuest/TasQuest/TasQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasQuestApp.swift
+//  TasQuest
+//
+//  Created by ITS on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct TasQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

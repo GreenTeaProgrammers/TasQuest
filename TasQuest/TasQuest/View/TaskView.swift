b36ae40e2354ad9ca5dd3fe703cbe61a53589689
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-05 21:09:54 +0900

    feat: Added the function to draw lines between tasks.

--- TasQuest/TasQuest/View/TaskView.swift
+++ TasQuest/TasQuest/View/TaskView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+
 struct TaskView: View {
     var goal: Goal
-    
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
         VStack {
-            // Goal情報表示エリア
-            Text(goal.name)
-                .font(.title)
-                .padding(.top)
+            ZStack(alignment: .leading) {
+                Text(goal.name)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .center)  // 中央に配置
+                
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.blue)
+                }
+                .padding(.leading)  // 左側に少し余白を追加
+            }
+            .padding(.top)
             
             Text(goal.dueDate)
                 .font(.subheadline)
@@ -23,36 +37,37 @@ struct TaskView: View {
             
             // Taskリスト表示エリア
             ScrollView {
-                // 縦の線を描画
-                VStack(alignment: .leading) {
-                    ForEach(goal.tasks.indices, id: \.self) { index in
-                        HStack {
-                            // 円形イメージ (ここではシステムイメージを使用)
-                            Image(systemName: "circle.fill")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                                .foregroundColor(.blue)
-                            
-                            // 線を描画
-                            if index < goal.tasks.count - 1 {
-                                VStack {
-                                    Divider()
-                                        .background(Color.blue)
+                ZStack(alignment: .leading) {
+                    // 縦の線を描画
+                    VStack {
+                        Rectangle()
+                            .fill(Color.black)
+                            .frame(width: 5, height: CGFloat(goal.tasks.count - 1) * (64 + CGFloat(goal.tasks.count - 1) * 0.5))  // 64は各HStackの高さ
+                    }
+                    .padding(.leading, 9)  // 9は微調整の結果
+
+                    // 各タスクと円
+                    VStack(alignment: .leading) {
+                        ForEach(goal.tasks.indices, id: \.self) { index in
+                            HStack {
+                                // 円形イメージ
+                                Image(systemName: "circle.fill")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(.blue)
+
+                                VStack(alignment: .leading) {
+                                    Text(goal.tasks[index].name)
+                                        .font(.body)
+                                    
+                                    Text(goal.tasks[index].dueDate)
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
                                 }
-                                .frame(height: 24)
+                                Spacer()
                             }
-                            
-                            // タスク情報
-                            VStack(alignment: .leading) {
-                                Text(goal.tasks[index].name)
-                                    .font(.body)
-                                
-                                Text(goal.tasks[index].dueDate)
-                                    .font(.caption)
-                                    .foregroundColor(.gray)
-                            }
+                            .frame(height: 64)  // 各HStackの高さを固定
                         }
-                        .padding(.bottom)
                     }
                 }
             }

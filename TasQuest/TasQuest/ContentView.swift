cad0b878f40feb57c9f0f1d9029aee5bd33d17d3
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-05 18:32:37 +0900

    feat: Added a View to display tasks(simplified).

--- TasQuest/TasQuest/ContentView.swift
+++ TasQuest/TasQuest/ContentView.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        let sampleGoal = Goal(
+            id: 1,
+            name: "Sample Goal",
+            description: "",
+            tasks: [
+                Task(id: 1, name: "Task 1", description: "", dueDate: "2023-09-30", maxHealth: 0, currentHealth: 0, tags: [], isVisible: true, createdAt: "", updatedAt: ""),
+                Task(id: 2, name: "Task 2", description: "", dueDate: "2023-10-05", maxHealth: 0, currentHealth: 0, tags: [], isVisible: true, createdAt: "", updatedAt: "")
+            ],
+            currentTaskIndex: 0,
+            dueDate: "2023-12-31",
+            isStarred: false,
+            tags: [],
+            thumbnail: "",
+            createdAt: "",
+            updatedAt: ""
+        )
+        
+        TaskView(goal: sampleGoal)
     }
 }
--- TasQuest/TasQuest/View/TaskView.swift
+++ TasQuest/TasQuest/View/TaskView.swift
@@ -1,8 +1,62 @@
 //
-//  TaskView.swift
+//  ContentView.swift
 //  TasQuest
 //
-//  Created by KinjiKawaguchi on 2023/09/05.
+//  Created by KinjiKawaguchi on 2023/09/04.
 //
 
-import Foundation
+import SwiftUI
+
+struct TaskView: View {
+    var goal: Goal
+    
+    var body: some View {
+        VStack {
+            // Goal情報表示エリア
+            Text(goal.name)
+                .font(.title)
+                .padding(.top)
+            
+            Text(goal.dueDate)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            // Taskリスト表示エリア
+            ScrollView {
+                // 縦の線を描画
+                VStack(alignment: .leading) {
+                    ForEach(goal.tasks.indices, id: \.self) { index in
+                        HStack {
+                            // 円形イメージ (ここではシステムイメージを使用)
+                            Image(systemName: "circle.fill")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.blue)
+                            
+                            // 線を描画
+                            if index < goal.tasks.count - 1 {
+                                VStack {
+                                    Divider()
+                                        .background(Color.blue)
+                                }
+                                .frame(height: 24)
+                            }
+                            
+                            // タスク情報
+                            VStack(alignment: .leading) {
+                                Text(goal.tasks[index].name)
+                                    .font(.body)
+                                
+                                Text(goal.tasks[index].dueDate)
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        .padding(.bottom)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}

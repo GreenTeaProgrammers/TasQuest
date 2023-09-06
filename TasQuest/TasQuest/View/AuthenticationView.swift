2725e8c30bae3d19799ea53592423c0185b46711
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-06 18:21:48 +0900

    feat: Create simple authenticationView

--- TasQuest/TasQuest/View/AuthenticationView.swift
+++ TasQuest/TasQuest/View/AuthenticationView.swift
@@ -5,4 +5,33 @@
 //  Created by KinjiKawaguchi on 2023/09/06.
 //
 
-import Foundation
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack{
+            NavigationLink{
+                Text("Hello")
+            } label: {
+                Text("Sign In With Email")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In")
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View{
+        NavigationStack{
+            AuthenticationView()
+        }
+    }
+}

bb94b38865d2a6d971388ef59a2c1b75c374b8ed
KikyoNanakusa <nanakusa.vrc@gmail.com>
2023-09-08 17:40:13 +0900

    feat: Implemented function that Loads Tasks and each task's information

--- Unity/Assets/Scripts/FireBase/FireStoreManager.cs
+++ Unity/Assets/Scripts/FireBase/FireStoreManager.cs
@@ -10,14 +10,48 @@ public class FireStoreManager : MonoBehaviour
 {
     public async void Awake()
     {
-        var res = await ReadFireStore();
-        foreach (var i in res.Keys)
-        {
-            Debug.Log($"{i} : {res[i]}");
-        }
+        // var res = await ReadFireStore("", "", "", "");
+        // foreach (var i in res.Keys)
+        // {
+        //     Debug.Log($"{i} : {res[i]}");
+        // }
+        
+        tasks();
     }
 
-    public async Task<Dictionary<string, object>> ReadFireStore()
+    public async void tasks()
+    {
+        QuerySnapshot tasksSnapshot = await ReadTasks();
+        foreach (var taskDocument in tasksSnapshot.Documents)
+        {
+            Dictionary<string, object> task = taskDocument.ToDictionary();
+
+            Debug.Log("-----------------------------------------");
+            Debug.Log($"Task ID(key) : {taskDocument.Id}");
+            foreach (var j in task.Keys)
+            {
+                Debug.Log($"{j} : {task[j]}");
+            }
+            
+            Debug.Log("----------------------------------------");
+        }
+    }
+    
+    public async Task<QuerySnapshot> ReadTasks()
+    {
+        FirebaseFirestore dataBase = FirebaseFirestore.DefaultInstance;
+        Query query = dataBase.Collection("Users")
+            .Document("RCGhBVMyFfaUIx7fwrcEL5miTnW2")
+            .Collection("Statuses")
+            .Document("1")
+            .Collection("Goals")
+            .Document("h8eWfbP3NR5tz81maI9p")
+            .Collection("TasQuestTasks");
+        var snapshot = await query.GetSnapshotAsync();
+        return snapshot;
+    }
+    
+    public async Task<Dictionary<string, object>> ReadFireStore(string usersDocument, string statusDocument, string goalDocument, string taskDocument)
     {
         FirebaseFirestore dataBase = FirebaseFirestore.DefaultInstance;
         DocumentReference docref = dataBase.Collection("Users")
@@ -31,6 +65,5 @@ public class FireStoreManager : MonoBehaviour
         Dictionary<string, object> result = snapshot.ToDictionary();
         
         return result;
-
     }
 }

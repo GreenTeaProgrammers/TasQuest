379474de9ce3ea3f8392946c36a2e614f14a81b7
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-08 13:52:45 +0900

    feat: Add view & viewModel file about Status

---
+++ TasQuest/TasQuest/View/StatusView.swift
@@ -0,0 +1,20 @@
+//
+//  StatusView.swift
+//  TasQuest
+//
+//  Created by KAWAGUCHI KINJI on 2023/09/08.
+//
+
+import SwiftUI
+
+struct StatusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatusView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusView()
+    }
+}
---
+++ TasQuest/TasQuest/ViewModel/StatusViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StatusViewModel.swift
+//  TasQuest
+//
+//  Created by KAWAGUCHI KINJI on 2023/09/08.
+//
+
+import Foundation

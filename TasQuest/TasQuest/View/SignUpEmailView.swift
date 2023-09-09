c43ee03a0a8f5c4b13290e74c2a407373a72ade5
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-10 03:25:06 +0900

    feat: Add SignUpEmail View and ViewModel

---
+++ TasQuest/TasQuest/View/SignUpEmailView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpEmailView.swift
+//  TasQuest
+//
+//  Created by ITS on 2023/09/10.
+//
+
+import SwiftUI
+
+struct SignUpEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpEmailView()
+    }
+}
---
+++ TasQuest/TasQuest/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  TasQuest
+//
+//  Created by ITS on 2023/09/10.
+//
+
+import Foundation

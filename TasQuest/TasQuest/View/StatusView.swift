54a2f1efe36bffd3bdac603928ace1ff87b66cd4
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-09 12:03:24 +0900

    feat: ゴールの表示をモダンなデザインに変更

--- TasQuest/TasQuest/View/StatusView.swift
+++ TasQuest/TasQuest/View/StatusView.swift
@@ -8,22 +8,47 @@
 
 import SwiftUI
 
+struct GoalRow: View {
+    var goal: Goal // Goal はあなたのモデルです
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(goal.name)
+                Text(goal.dueDate)
+            }
+
+            Spacer()
+
+            Button(action: {
+                // Star button tapped
+            }) {
+                Image(systemName: goal.isStarred ? "star.fill" : "star")
+                    .foregroundColor(goal.isStarred ? .yellow : .gray)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(Color.white) // 背景を白に設定
+        .cornerRadius(10) // 角を丸くする
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2) // 影をつける
+        .overlay(
+            NavigationLink("", destination: Text("Status View")) // ここで遷移先を設定します
+                .opacity(0) // NavigationLink を透明にする
+        )
+        .padding(.bottom, 8)
+    }
+}
+
 struct StatusView: View {
     @State private var showSignInView: Bool = false
     @State private var showSettingView: Bool = false
 
+    @StateObject private var viewModel = StatusViewModel()
+    
     // ダミーデータ
-    let user: AppData = setDummyData()
-
-    func backgroundColor(for index: Int) -> Color {
-        switch index {
-        case 0: return Color.red
-        case 1: return Color.green
-        case 2: return Color.blue
-        default: return Color.gray
-        }
-    }
-
+    @State private var user: AppData = AppData()
+    
     var body: some View {
         VStack {
             // ウェルカムメッセージと設定ボタン
@@ -45,65 +70,40 @@ struct StatusView: View {
             .padding()
 
             // ステータスとその目標を表示
-            ForEach(user.statuses.indices, id: \.self) { index in
-                let status = user.statuses[index]
+            ScrollView {
+                ForEach(user.statuses.indices, id: \.self) { index in
+                    let status = user.statuses[index]
 
-                VStack {
-                    Text(status.name)
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding(.top)
+                    VStack {
+                        Text(status.name)
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.top)
 
-                    ForEach(status.goals, id: \.id) { goal in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(goal.name)
-                                Text(goal.dueDate)
-                            }
-
-                            Spacer()
-
-                            Button(action: {
-                                // Star button tapped
-                            }) {
-                                Image(systemName: goal.isStarred ? "star.fill" : "star")
-                                    .foregroundColor(goal.isStarred ? .yellow : .gray)
-                            }
+                        ForEach(status.goals, id: \.id) { goal in
+                            GoalRow(goal: goal)
                         }
-                        .padding(.horizontal)
                     }
+                    .padding()
+                    .background(viewModel.backgroundColor(for: index))
+                    .cornerRadius(15)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 1)
+                            .foregroundColor(.gray)
+                    )
+                    .padding(.bottom)
                 }
-                .padding()
-                .background(backgroundColor(for: index))
-                .cornerRadius(15)
-                .padding(.bottom)
             }
         }
         .fullScreenCover(isPresented: $showSignInView) {
             AuthenticationView(showSignInView: $showSignInView)
         }
         .onAppear() {
+            user = viewModel.setDummyData()
             let authUser = try? AuthenticationManager.shared.getAuthenticatedUser()
             self.showSignInView = authUser == nil
         }
         Spacer()
     }
-}
-
-
-
-
-func setDummyData() -> AppData{
-    return AppData(userid: "1", username: "Kinji", statuses: [
-        Status(id: "1", name: "対応前", goals: [
-            Goal(id: "1", name: "Goal 1", description: "", tasks: [], dueDate: "2023-09-20", isStarred: false, tags: [], thumbnail: "", createdAt: "", updatedAt: ""),
-            Goal(id: "2", name: "Goal 2", description: "", tasks: [], dueDate: "2023-10-01", isStarred: true, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
-        ], updatedAt: ""),
-        Status(id: "2", name: "対応中", goals: [
-            Goal(id: "3", name: "Goal 3", description: "", tasks: [], dueDate: "2023-09-22", isStarred: false, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
-        ], updatedAt: ""),
-        Status(id: "3", name: "完了", goals: [
-            Goal(id: "4", name: "Goal 4", description: "", tasks: [], dueDate: "2023-09-19", isStarred: true, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
-        ], updatedAt: "")
-    ], tags: [])
 }
--- TasQuest/TasQuest/ViewModel/StatusViewModel.swift
+++ TasQuest/TasQuest/ViewModel/StatusViewModel.swift
@@ -6,18 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 class StatusViewModel: ObservableObject {
-    @Published var goal: Goal
     
-    init(goal: Goal) {
-        self.goal = goal
+    func setDummyData() -> AppData{
+        return AppData(userid: "1", username: "Kinji", statuses: [
+            Status(id: "1", name: "未着手", goals: [
+                Goal(id: "1", name: "Goal 1", description: "", tasks: [], dueDate: "2023-09-20", isStarred: false, tags: [], thumbnail: "", createdAt: "", updatedAt: ""),
+                Goal(id: "2", name: "Goal 2", description: "", tasks: [], dueDate: "2023-10-01", isStarred: true, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
+            ], updatedAt: ""),
+            Status(id: "2", name: "対応中", goals: [
+                Goal(id: "3", name: "Goal 3", description: "", tasks: [], dueDate: "2023-09-22", isStarred: false, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
+            ], updatedAt: ""),
+            Status(id: "3", name: "完了", goals: [
+                Goal(id: "4", name: "Goal 4", description: "", tasks: [], dueDate: "2023-09-19", isStarred: true, tags: [], thumbnail: "", createdAt: "", updatedAt: "")
+            ], updatedAt: "")
+        ], tags: [])
     }
     
-    func toggleIsStarred() -> Goal{
-        self.goal.isStarred.toggle()
-        return self.goal
-        // Todo: ここでデータベースへの保存などの追加処理を行うこともできます。
+    func backgroundColor(for index: Int) -> Color {
+        switch index {
+        case 0: return Color(red: (255-30)/255, green: (226-30)/255, blue: (221-30)/255).opacity(1) // 赤寄りのピンク、少し濃い
+        case 1: return Color(red: (253-30)/255, green: (236-30)/255, blue: (200-30)/255).opacity(1) // 黄色よりのオレンジ、少し濃い
+        case 2: return Color(red: (219-30)/255, green: (237-30)/255, blue: (219-30)/255).opacity(1)
+        default: return Color.gray
+        }
     }
 }

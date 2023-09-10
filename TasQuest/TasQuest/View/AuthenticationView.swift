1e23d059b60d55bd2b900770f724c7a99e8a23ee
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-10 15:38:52 +0900

    fix: AuthenticationViewが正しく表示されるように修正

--- TasQuest/TasQuest/View/AuthenticationView.swift
+++ TasQuest/TasQuest/View/AuthenticationView.swift
@@ -26,45 +26,37 @@ struct AuthenticationView: View {
                     .foregroundColor(.white)
                     .padding(.bottom, 50)
                 
+                
                 // Sign In Button
                 NavigationLink(destination: SignInEmailView(showSignInView: $showSignInView)) {
-                    Text("Sign In With Email")
+                    Text("メールアドレスでサインイン")
                         .font(.headline)
                         .foregroundColor(.white)
-                        .padding(.bottom, 50)
-                    
-                    // Sign In Button
-                    NavigationLink(destination: SignInEmailView(showSignInView: $showSignInView)) {
-                        Text("メールアドレスでサインイン")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(height: 55)
-                            .frame(maxWidth: .infinity)
-                            .background(Color.green)
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
-                            .padding(.horizontal, 20)
-                    }
-                    .padding(.top, 20)
-                    
-                    // Sign Up Button
-                    NavigationLink(destination: SignUpEmailView(showSignInView: $showSignInView)) { // Assuming you have a SignUpEmailView
-                        Text("メールアドレスで登録")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(height: 55)
-                            .frame(maxWidth: .infinity)
-                            .background(Color.orange)
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
-                            .padding(.horizontal, 20)
-                    }
-                    .padding(.top, 20)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding(.horizontal, 20)
                 }
                 .padding(.top, 20)
                 
+                // Sign Up Button
+                NavigationLink(destination: SignUpEmailView(showSignInView: $showSignInView)) { // Assuming you have a SignUpEmailView
+                    Text("メールアドレスで登録")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.orange)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .padding(.horizontal, 20)
+                }
+                .padding(.top, 20)
             }
-            .padding()
+            .padding(.top, 20)
+            
         }
         .navigationTitle("Sign In")
         .navigationBarHidden(true)

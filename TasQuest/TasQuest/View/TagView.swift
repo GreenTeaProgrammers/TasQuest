8e41e9e8733cf79270930d799c82823709285e60
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-12 09:42:05 +0900

    feat: Create TagView.swift

---
+++ TasQuest/TasQuest/View/TagView.swift
@@ -0,0 +1,14 @@
+//
+//  TagView.swift
+//  TasQuest
+//
+//  Created by KinjiKawaguchi on 2023/09/12.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}

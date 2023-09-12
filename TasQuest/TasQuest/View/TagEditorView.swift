3b7e6e69141c494134b018e334cc84a37d5f4bfe
KinjiKawaguchi <kawakin0310@icloud.com>
2023-09-12 09:48:56 +0900

    feat: Create TagEditorView.swift

---
+++ TasQuest/TasQuest/View/TagEditorView.swift
@@ -0,0 +1,20 @@
+//
+//  TagEditorView.swift
+//  TasQuest
+//
+//  Created by KijiKawaguchi on 2023/09/12.
+//
+
+import SwiftUI
+
+struct TagEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagEditorView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagEditorView()
+    }
+}

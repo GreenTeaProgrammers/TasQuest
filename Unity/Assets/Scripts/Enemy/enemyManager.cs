13d15143a4fb792409413283f124fedb4557287e
KikyoNanakusa <nanakusa.vrc@gmail.com>
2023-09-13 09:55:34 +0900

    feat: Implement ChangeEnemyStrength method

--- Unity/Assets/Scripts/Enemy/enemyManager.cs
+++ Unity/Assets/Scripts/Enemy/enemyManager.cs
@@ -1,12 +1,11 @@
 using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
     private bool _isActive;
-    
-    private GameObject _mainCamera;
     private float _radius;
     private int _index;
     public int Index
@@ -15,9 +14,19 @@ public class EnemyManager : MonoBehaviour
         set { _index = value; }
     }
     
+    private GameObject _mainCamera;
+    private TMP_Text _hpText;
+    private GameObject _enemyWeak;
+    private GameObject _enemyNormal;
+    private GameObject _enemyStrong;
+    
     void Start()
     {
         _mainCamera = GameObject.Find("Main Camera");
+        _hpText = GameObject.Find("HpText").GetComponent<TMP_Text>();
+        _enemyWeak = this.transform.GetChild(1).gameObject;
+        _enemyNormal = this.transform.GetChild(2).gameObject;
+        _enemyStrong = this.transform.GetChild(3).gameObject;
     }
     
     void Update()
@@ -30,13 +39,45 @@ public class EnemyManager : MonoBehaviour
                 this.transform.position.y, 
                 targetTransform.z));
 
-        if (Index == CameraMovement.CurrentIndex)
+        //_indexとカメラのcurrentIndexが等しいときアクティブ
+        _isActive = Index == CameraMovement.CurrentIndex;
+        if (_isActive)
         {
-            _isActive = true;
+            int maxHp = int.Parse(_hpText.text.Split("/")[1]);
+            if (maxHp < 500)
+            {
+                ChangeEnemyStrength("weak");
+            }
+            else if(maxHp < 1000)
+            {
+                ChangeEnemyStrength("normal");
+            }
+            else
+            {
+                ChangeEnemyStrength("strong");
+            }
+        }
+    }
+
+    private void ChangeEnemyStrength(string strength)
+    {
+        if (strength == "weak")
+        {
+            _enemyWeak.SetActive(true);
+            _enemyNormal.SetActive(false);
+            _enemyStrong.SetActive(false);
+        }
+        else if (strength == "normal")
+        {
+            _enemyWeak.SetActive(false);
+            _enemyNormal.SetActive(true);
+            _enemyStrong.SetActive(false);
         }
         else
         {
-            _isActive = false;
+            _enemyWeak.SetActive(false);
+            _enemyNormal.SetActive(false);
+            _enemyStrong.SetActive(true);
         }
     }
     
